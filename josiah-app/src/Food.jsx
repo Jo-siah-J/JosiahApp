872e1426f96f7b671c08e572eeaf95e015279f27
Josiah Williams <Jos2293687@gmail.com>
2026-01-06 22:30:04 -0500

    initial commit

---
+++ josiah-app/src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food(){
+
+    const food1 = "Mango";
+    const food2 = "Cherry";
+
+    return(
+        <ul>
+            <li>Orange</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food
